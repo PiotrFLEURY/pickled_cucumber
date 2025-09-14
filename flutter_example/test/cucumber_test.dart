228b63bce9fc3136418901ff63566d2b3fdb4fbd
PiotrFLEURY <piotr.fleury@gmail.com>
2025-09-14 15:32:00 +0200

    chore(1.3.0): prepare for release

--- flutter_example/test/cucumber_test.dart
+++ flutter_example/test/cucumber_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'counter_step_definitions.pickled.dart' as counter;
 import 'math_step_definitions.pickled.dart' as math;
 import 'jira_step_definitions.pickled.dart' as jira;
+import 'background_account_step_definitions.pickled.dart' as background;
 
 void main() {
   group('Counter', () {
@@ -13,5 +14,8 @@ void main() {
   });
   group('Jira', () {
     jira.runFeatures();
+  });
+  group('Background Account', () {
+    background.runFeatures();
   });
 }
