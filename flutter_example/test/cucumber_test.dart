7b8f4e2ae515ea2755e14b4fc3be45e659eece3c
PiotrFLEURY <piotr.fleury@gmail.com>
2026-02-07 15:45:14 +0100

    fix(28): fix step identification for only parametrized scenarios

--- flutter_example/test/cucumber_test.dart
+++ flutter_example/test/cucumber_test.dart
@@ -4,6 +4,8 @@ import 'counter_step_definitions.pickled.dart' as counter;
 import 'math_step_definitions.pickled.dart' as math;
 import 'jira_step_definitions.pickled.dart' as jira;
 import 'background_account_step_definitions.pickled.dart' as background;
+import 'library_step_definitions.pickled.dart' as library_steps;
+import 'only_parameter_step_definitions.pickled.dart' as only_parameter;
 
 void main() {
   group('Counter', () {
@@ -17,5 +19,11 @@ void main() {
   });
   group('Background Account', () {
     background.runFeatures();
+  });
+  group('Library', () {
+    library_steps.runFeatures();
+  });
+  group('Only Parameter', () {
+    only_parameter.runFeatures();
   });
 }
